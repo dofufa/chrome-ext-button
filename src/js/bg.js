a3b69ad1cbd8f41e1f6e0e502daefd01bae3e7eb
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2024-02-19 08:19:50 -0800

    Update bg.js

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,1 +1,8 @@
-console.log('ext-bg-hello');
+console.log('ext-button-hello-bg');
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["js/ext.js"]
+  });
+});
